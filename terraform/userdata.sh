207f970f81caf12a661f80c1478b1d7f226d8e45
Obsidian <obsidianmaximus@protonmail.com>
2025-07-26 17:32:20 +0530

    Try 3

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -31,12 +31,12 @@ fi
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 LOG_DIR="s3://${S3_BUCKET_NAME}/ec2-system-logs/$TIMESTAMP"
 
-echo "Uploading system logs to ${LOG_DIR}" >> /tmp/shutdown.log
+echo "Uploading system logs to $LOG_DIR" >> /tmp/shutdown.log
 
 # Upload the cloud-init log and syslog to the specified S3 path.
 # The EC2 instance has permission to do this via the IAM role attached to it.
-aws s3 cp /var/log/cloud-init.log "${LOG_DIR}/cloud-init.log" >> /tmp/shutdown.log 2>&1
-aws s3 cp /var/log/syslog "${LOG_DIR}/syslog" >> /tmp/shutdown.log 2>&1
+aws s3 cp /var/log/cloud-init.log "$LOG_DIR/cloud-init.log" >> /tmp/shutdown.log 2>&1
+aws s3 cp /var/log/syslog "$LOG_DIR/syslog" >> /tmp/shutdown.log 2>&1
 
 echo "Log upload complete." >> /tmp/shutdown.log
 EOF
