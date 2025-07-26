5cfdeb27966d0d4e63548900155313e868dc936f
Obsidian <obsidianmaximus@protonmail.com>
2025-07-26 14:56:37 +0530

    Introduce stage variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,13 @@
 resource "aws_instance" "techeazy-devops" {
-  ami           = var.ami_number # Ubuntu 24.04
-  instance_type = "t2.micro"
-  security_groups = [aws_security_group.allow_http.name]
+  ami           = var.ami_number
+  instance_type = var.instance_type # Use the variable here
+  security_groups = [aws_security_group.allow_http_ssh.name]
   iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
-  user_data = "${file("userdata.sh")}"
+  user_data = file("userdata.sh")
 
   tags = {
     Name = "techeazy-SSM-Managed-Instance"
   }
-
 }
 
 output "ec2_instance_id" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,9 @@ variable "ami_number" {
   default = "ami-0fcfcdc5efc25e0bc"
   type    = string
 }
+
+variable "instance_type" {
+  description = "The EC2 instance type to use for the environment."
+  type        = string
+  default     = "t2.micro" # Default to the smallest size.
+}
