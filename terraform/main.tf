ec8bc467e81c1b093b1ba08c65b62c80197be696
Obsidian <obsidianmaximus@protonmail.com>
2025-07-26 20:49:04 +0530

    feat. Implement s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,11 @@ resource "aws_instance" "techeazy-devops" {
   }
 }
 
+resource "aws_iam_instance_profile" "s3_instance_profile" {
+  name = "S3InstanceProfile"
+  role = aws_iam_role.s3_write_role.name
+}
+
 output "ec2_instance_id" {
   value = aws_instance.techeazy-devops.id
 }
